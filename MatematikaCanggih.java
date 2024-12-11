96c6e9b99e4ea971b9063770b53575c194c716a8
Dendireme <dendyreme340@gmail.com>
2024-12-11 21:40:03 +0700

    Add files via upload

--- MatematikaCanggih.java
+++ MatematikaCanggih.java
@@ -2,26 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package matematika;
 
 /**
  *
- * @author ASUS
+ * @author USER
  */
-class Matematika {
-    // Metode untuk pertambahan
-    public int pertambahan(int a, int b) {
-        return a + b;
-    }
-
-    // Metode untuk perkalian
-    public int perkalian(int a, int b) {
-        return a * b;
-    }
-}
-
-public class MatematikaCanggih extends Matematika {
-    // Metode untuk menghitung modulus
-    public int modulus(int a, int b) {
-        return a % b;
+public class matematikacanggih extends matematika {
+    //method tambahan
+    public double operasiTigaAngkah(double a, double b, double c){
+        return a + b - c; // operasi gabungan
     }
 }
